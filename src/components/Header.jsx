e3d1bf0113ca7f3ad40ec6beaaec09086e4fd256
CHELIO1902 <chelio19022012@hotmail.com>
2022-07-21 20:06:09 -0500

    actualizando el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,26 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav>
-      
-      <ul className="nav-links">
-        <Link to='/'>
-          <li>Home</li>
-        </Link>
+    <div className='container d-flex justify-content-center'>
+      <nav className=' bg-dark '>
+        <div>
+          
+        </div>
+        <ul className="nav-links">
+          </div>
+            <Link to='/'>
+              <li>Home</li>
+            </Link>
+          <div>
+            
 
-        <Link to='user-screen'>
-          <li>Items</li>
-        </Link>
+          <Link to='user-screen'>
+            <li>Items</li>
+          </Link>
 
-      </ul>
+        </ul>
 
-    </nav>
+      </nav>
+    </div>
   )
 }
